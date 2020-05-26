8ba6067e1c6654593b91ba167ff18591f5a77833
Ulder Carrilho Júnior <uldercarrilho@gmail.com>
2020-05-26 12:33:36 -0300

    Inclusão do Jump Statement continue

---
+++ src/StatementsExpressionsOperators/Statements/Jump/JumpStatements_continue.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Statements.Jump
+{
+    public class JumpStatements_continue
+    {
+        // The continue statement passes control to the next iteration of the enclosing while, do, for, or foreach statement in which it appears.
+
+        public static void Example1()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i < 9)
+                {
+                    continue;
+                }
+                Console.WriteLine(i);
+            }            
+        }
+    }
+}
