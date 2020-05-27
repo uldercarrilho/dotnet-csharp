8b858a076ca6c815f35e396d3690d188ea178ee4
Ulder Carrilho Júnior <uldercarrilho@gmail.com>
2020-05-27 16:10:49 -0300

    Inclusão da solução Types

---
+++ src/Types/TypesProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TypesProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
