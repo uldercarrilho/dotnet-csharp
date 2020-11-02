f508a99550bb8982b19b419f6a42fe845d3a5967
Ulder Carrilho Júnior <uldercarrilho@gmail.com>
2020-11-02 08:57:35 -0300

    Inclusão da solução File System And Registry (unificado com os commits VSCODE)

--- src/FileSystemAndRegistry/FileSystemProject/Program.cs
+++ src/FileSystemAndRegistry/FileSystemProject/Program.cs
@@ -7,6 +7,20 @@ namespace FileSystemProject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            
+            // How to iterate through a directory tree
+            RecursiveFileSearch.Run();
+            StackBasedIteration.Run(@"C:\SAJ");
+            
+            FileSysInfo.Run();
+            CreateFileOrFolder.Run();
+            SimpleFileCopy.Run();
+            SimpleFileMove.Run();
+            SimpleFileDelete.Run();
+            WriteTextFile.Run();
+            ReadFromFile.Run();
+            // How to create a key in the registry
+            RegistryTest.Run();
         }
     }
 }
